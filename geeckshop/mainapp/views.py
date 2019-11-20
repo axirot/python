445f32e6704f8651af28ea7bde747cd0ec6b5c91
Иван Карпов <ivan.karpov@shtormtech.ru>
2019-11-20 17:46:43 +0300

    lesson5

--- geeckshop/mainapp/views.py
+++ geeckshop/mainapp/views.py
@@ -1,26 +1,26 @@
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Product, ProductCategory
 
-from django.shortcuts import render
-import json, os
-from mainapp.models import Product, ProductCategory
-
-JSON_PATH = 'mainapp/json'
-
-def loadMenuFromJSON():
-    with open(os.path.join(JSON_PATH, 'menu.json'), 'r') as infile:
-        return json.load(infile)
 
 def main(request):
-    links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu, 'username' :  'ivan', 'array' : [1, 2, 3, 4, 5]}
+    context = {'username': 'alexey'}
     return render(request, 'mainapp/main.html', context)
 
-def products(request):
-    links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu, 'products': Product.objects.all()}
-    return render(request, 'mainapp/products.html', context)
+
+def products(request, pk=None):
+    products = Product.objects.all()
+
+    if pk or pk == 0:
+        if pk != 0:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = products.filter(category=category)
+        context = {'products': products, 'categories': ProductCategory.objects.all()}
+        return render(request, 'mainapp/products.html', context)
+    else:
+        hot_product = Product.objects.filter(is_hot=True).first()
+        context = {'hot_product': hot_product, 'categories': ProductCategory.objects.all()}
+        return render(request, 'mainapp/hot_product.html', context)
+
 
 def contacts(request):
-    links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu}
-    return render(request, 'mainapp/contacts.html', {'title': 'Kontakti'})
+    return render(request, 'mainapp/contacts.html')
