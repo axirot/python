648fad872350874e41d53a097a3b0233ae426dea
axirot <axiros@yandex.ru>
2019-11-29 21:37:58 +0400

    6 lesson

--- geeckshop/authapp/views.py
+++ geeckshop/authapp/views.py
@@ -5,25 +5,42 @@ from django.urls import reverse, reverse_lazy
 from .forms import ShopUserRegisterForm
 from .models import ShopUser
 
+
 def register(request):
-    form = ShopUserRegisterForm()
+
     if request.method == 'POST':
-        form = ShopUserRegisterForm(request.POST, request.FILES)
-        if form.is_valid():
-            user = form.save()
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if register_form.is_valid():
+            user = register_form.save()
             auth.login(request, user)
             return HttpResponseRedirect(reverse('main'))
-    return render(request, 'authapp/register.html', {'form': form})
+    else:
+        register_form = ShopUserRegisterForm()
+
+    context = {
+        'form': register_form,
+        'title': 'Register',
+        'submit_label': 'Zergistrirovatsya'
+    }
+    return render(request, 'authapp/register.html', context)
+
 
 def login(request):
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        next = request.POST.get('next')
         user = auth.authenticate(username=username, password=password)
-        if user:
+        if user and user.is_active:
             auth.login(request, user)
-            return HttpResponseRedirect(reverse('main'))
-    return render(request, 'authapp/login.html')
+            if next:
+                return HttpResponseRedirect(next)
+            else:
+                return HttpResponseRedirect(reverse('main'))
+
+    return render(request, 'authapp/login.html', {'title': 'Enter', 'next': request.GET.get('next')})
+
 
 def logout(request):
     auth.logout(request)
@@ -33,5 +50,11 @@ def logout(request):
 class EditView(UpdateView):
     model = ShopUser
     template_name = 'authapp/register.html'
-    fields = 'username', 'firstname', 'avatar'
+    fields = 'username', 'email', 'avatar'
     success_url = reverse_lazy('main')
+
+    def get_context_data(self, **kwargs):
+        context = super(EditView, self).get_context_data(**kwargs)
+        context['title'] = 'Redaktirovat profile'
+        context['submit_label'] = 'Primenit'
+        return context
--- geeckshop/mainapp/models.py
+++ geeckshop/mainapp/models.py
@@ -12,16 +12,17 @@ class ProductCategory(models.Model):
 
 class Product(models.Model):
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    name = models.CharField(verbose_name='имя продукта', max_length=128)
+    name = models.CharField(verbose_name='imya producta', max_length=128)
     image = models.ImageField(upload_to='products_images', blank=True)
-    short_desc = models.CharField(verbose_name='кратко', max_length=60, blank=True)
-    description = models.TextField(verbose_name='подробно', blank=True)
-    price = models.DecimalField(verbose_name='цена', max_digits=8, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField(verbose_name='склад', default=0)
+    short_desc = models.CharField(verbose_name='kratko', max_length=60, blank=True)
+    description = models.TextField(verbose_name='podrobno', blank=True)
+    price = models.DecimalField(verbose_name='cena', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='sklad', default=0)
+    is_hot = models.BooleanField(verbose_name='goryachi product', default=False)
 
     def __str__(self):
         return "{} ({})".format(self.name, self.category.name)
